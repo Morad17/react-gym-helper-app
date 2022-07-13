55d0221e153b57e776542875bed0299a1cbc0b64
Morad17 <morad123@hotmail.co.uk>
2022-07-13 20:21:54 +0100

    started updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
+import axios from 'axios'
 
 const Home = () => {
+    
+const getFoodInfo = async ({})  => {
+  try {
+    const data = await axios.get('https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data', {
+      params: {},
+      headers: {
+        'x-rapidapi-host': 'edamam-edamam-nutrition-analysis.p.rapidapi.com',
+        'x-rapidapi-key': process.env.APP_KEY
+      }
+    
+    })
+    return data
+    
+  } catch (err){
+      console.log(err)
+    }
+}
+  console.log(data);
   return (
     <div>Home</div>
   )
